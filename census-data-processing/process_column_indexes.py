733490f545868d97ce16157539b28fd2e146c99f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-04-22 01:49:58 -0600

    format column headings as java constants

--- census-data-processing/process_column_indexes.py
+++ census-data-processing/process_column_indexes.py
@@ -5,12 +5,14 @@ inputFile.close()
 
 i = 0
 output = ''
+java_output = ''
 for i in range(len(splits)):
     output += str(i) + ' ' + splits[i].strip() + '\n'
+    java_output += 'public static final int ' + splits[i].strip().replace(' ', '_').upper() + ' = ' + str(i) + ';\n'
     i += 1
 
-outputFile = open('columns-w-indexes.txt', 'a')
-outputFile.write(output)
+outputFile = open('java-outputs.txt', 'a')
+outputFile.write(java_output)
 
 outputFile.flush()
 outputFile.close()
