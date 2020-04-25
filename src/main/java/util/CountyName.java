c40e94b055a0714eb67a63aa18dd89537573edee
laksheenmendis <cresclux@yahoo.com>
2020-04-25 16:02:11 -0600

    Analysis task for finding the hour with highest parking tickets per county

---
+++ src/main/java/util/CountyName.java
@@ -0,0 +1,10 @@
+package util;
+
+public class CountyName {
+
+    public static final String NEW_YORK_COUNTY = "NY";
+    public static final String KINGS_COUNTY = "K";
+    public static final String QUEENS_COUNTY = "Q";
+    public static final String BRONX_COUNTY = "BX";
+    public static final String RICHMOND_COUNTY = "R";
+}
