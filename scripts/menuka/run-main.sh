87dd6b59fc5362ce256df8e7c0161fb947fb67ac
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-04-25 18:45:21 -0600

    find distinct violation counties

--- scripts/menuka/run-main.sh
+++ scripts/menuka/run-main.sh
@@ -4,4 +4,4 @@ rm -rf target;
 sbt package;
 ${SPARK_HOME}/bin/spark-submit --master local --driver-memory 2g --executor-memory 2g \
 	--class census.Main \
-	target/scala-2.11/cs455-term-project_2.11-1.0.jar sample-data/2014.csv;
+	target/scala-2.11/cs455-term-project_2.11-1.0.jar sample-data/${1}.csv;
