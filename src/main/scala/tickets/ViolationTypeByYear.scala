71dd5837c00e3f7feb7f5551adb43448e0975d15
PhilipKirner <philip.kirner@gmail.com>
2020-04-27 18:34:58 -0600

    Getting a feel for spark

---
+++ src/main/scala/tickets/ViolationTypeByYear.scala
@@ -0,0 +1,16 @@
+package tickets
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import util.DataFields
+
+object ViolationTypeByYear {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("ParkingTicket0").getOrCreate()
+    val sc = SparkContext.getOrCreate()
+
+    val parkingData = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("hdfs://topeka:4056/cs455/park/*.csv")
+    val trimmedData = parkingData.select(DataFields.VIOLATION_CODE, DataFields.ISSUE_DATE)
+
+  }
+}
