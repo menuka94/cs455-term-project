cdf1ce3868c27de28aa3ba1358f428fa6a0208cb
PhilipKirner <philip.kirner@gmail.com>
2020-04-27 19:54:08 -0600

    do a grouped count

--- src/main/scala/tickets/ViolationTypeByYear.scala
+++ src/main/scala/tickets/ViolationTypeByYear.scala
@@ -12,5 +12,11 @@ object ViolationTypeByYear {
     val parkingData = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("hdfs://topeka:4056/cs455/park/*.csv")
     val trimmedData = parkingData.select(DataFields.VIOLATION_CODE, DataFields.ISSUE_DATE)
 
+    val output = trimmedData.groupBy(DataFields.VIOLATION_CODE, get_year(DataFields.ISSUE_DATE)).count
+
+  }
+
+  def get_year(str: String): String = {
+    str.substring(6)
   }
 }
