4d87e137f9e6a8c4a21c2c313706c1a3245406f0
PhilipKirner <philip.kirner@gmail.com>
2020-04-29 16:13:15 -0600

    try to transform a column for 3 hours, fail

--- src/main/scala/tickets/ViolationTypeByYear.scala
+++ src/main/scala/tickets/ViolationTypeByYear.scala
@@ -1,19 +1,36 @@
 package tickets
 
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
 import util.DataFields
 
 object ViolationTypeByYear {
-  def main(args: Array[String]) {
-    val spark = SparkSession.builder.appName("ParkingTicket0").getOrCreate()
-    val sc = SparkContext.getOrCreate()
+  val SEPARATOR = ":"
+  val YEAR_LIST = List("2013", "2014", "2015", "2016", "2017")
 
-    val parkingData = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("hdfs://topeka:4056/cs455/park/*.csv")
+  def main(args: Array[String]) {
+    if (args.length != 2) {
+      println("Usage ViolationTypeByYear <parking-tickets-file-path> <output-file>")
+      System.exit(0)
+    }
+
+    val PARKING_TICKETS_FILE_PATH = args(0)
+    val OUTPUT_FILE = args(1)
+    val NUMBER_OF_PARTITIONS = 100
+
+    val spark = SparkSession.builder.appName("ViolationTypeByYear").getOrCreate()
+    val sc = SparkContext.getOrCreate()
+    import spark.implicits._
+
+    val parkingData = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(PARKING_TICKETS_FILE_PATH/*"hdfs://topeka:4056/cs455/park/*.csv"*/*/)
     val trimmedData = parkingData.select(DataFields.VIOLATION_CODE, DataFields.ISSUE_DATE)
 
     val output = trimmedData.groupBy(DataFields.VIOLATION_CODE, get_year(DataFields.ISSUE_DATE)).count
-
+    //do an orderBy?
+    //https://stackoverflow.com/questions/37039943/spark-scala-how-to-transform-a-column-in-a-df
+    //https://stackoverflow.com/questions/33393815/count-instances-of-combination-of-columns-in-spark-dataframe-using-scala
+    //https://alvinalexander.com/scala/how-to-use-scala-match-expression-like-switch-case-statement/
+    //https://backtobazics.com/big-data/spark/apache-spark-groupby-example/
   }
 
   def get_year(str: String): String = {
