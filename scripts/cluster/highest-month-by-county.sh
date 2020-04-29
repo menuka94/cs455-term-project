1ca4dd20acd9abfbd042106e95c0695d123b3614
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-04-28 19:17:26 -0600

    add timestamps to output file names

--- scripts/cluster/highest-month-by-county.sh
+++ scripts/cluster/highest-month-by-county.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Define a timestamp function
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+
 rm -rf target
 sbt package
 ${SPARK_HOME}/bin/spark-submit --master yarn \
   --class tickets.HighestMonthByCounty \
   --deploy-mode cluster \
-  --supervise target/scala-2.11/cs455-term-project_2.11-1.0.jar /nyc /outputs
+  --supervise target/scala-2.11/cs455-term-project_2.11-1.0.jar /nyc /outputs/highest-month-by-county-$(timestamp)
--- scripts/local/highest-hour-by-county.sh
+++ scripts/local/highest-hour-by-county.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Define a timestamp function
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+
 rm -rf target;
 sbt package;
 ${SPARK_HOME}/bin/spark-submit --master local --driver-memory 2g --executor-memory 2g \
 	--class tickets.HighestHourByCounty \
-	target/scala-2.11/cs455-term-project_2.11-1.0.jar sample-data/${1}.csv sample-data/outputs;
+	target/scala-2.11/cs455-term-project_2.11-1.0.jar sample-data/ outputs/highest-hour-by-county-$(timestamp);
