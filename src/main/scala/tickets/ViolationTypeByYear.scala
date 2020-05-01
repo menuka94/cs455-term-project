7f37c437abe0428d41cde19bea5d23a7a66584ad
PhilipKirner <philip.kirner@gmail.com>
2020-04-30 18:13:20 -0600

    filter for valid parking violation codes, import parking violation codes

--- src/main/scala/tickets/ViolationTypeByYear.scala
+++ src/main/scala/tickets/ViolationTypeByYear.scala
@@ -3,9 +3,12 @@ package tickets
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import util.DataFields
+import util.CodesList
+import java.util
 
 object ViolationTypeByYear {
   val YEAR_LIST = List("2013", "2014", "2015", "2016", "2017")
+  val CODES_LIST: util.List[String] = CodesList.CODES_LIST
 
   def main(args: Array[String]) {
     if (args.length != 2) {
@@ -21,24 +24,26 @@ object ViolationTypeByYear {
     import spark.implicits._
 
     val parkingData = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(PARKING_TICKETS_FILE_PATH/*"hdfs://topeka:4056/cs455/park/*.csv"*/*/)
+
     val trimmedData = parkingData.select(DataFields.VIOLATION_CODE, DataFields.ISSUE_DATE)
 
     val yearlyData = trimmedData.withColumn("Year",$"Issue Date".substr(7,4)).select("Year", "Violation Code")
 
-    val output = yearlyData.groupBy("Year", "Violation Code").count.orderBy("Year")
+    val filteredYearlyData = yearlyData.filter($"Year" isin YEAR_LIST).filter($"Violation Code" isin CODES_LIST)
 
-    val filteredOutput = output.filter($"Year" isin YEAR_LIST)
+    val output = filteredYearlyData.groupBy("Year", "Violation Code").count.orderBy("Year", "Violation Code")
 
-    filteredOutput.collect()
+    val outputRDD = output.toJavaRDD
+
+    outputRDD.saveAsTextFile(OUTPUT_FILE)
+
+
+    //output.collect()
 
     //do an orderBy?
     //https://stackoverflow.com/questions/37039943/spark-scala-how-to-transform-a-column-in-a-df
     //https://stackoverflow.com/questions/33393815/count-instances-of-combination-of-columns-in-spark-dataframe-using-scala
     //https://alvinalexander.com/scala/how-to-use-scala-match-expression-like-switch-case-statement/
     //https://backtobazics.com/big-data/spark/apache-spark-groupby-example/
-  }
-
-  def get_year(str: String): String = {
-    str.substring(6)
   }
 }
